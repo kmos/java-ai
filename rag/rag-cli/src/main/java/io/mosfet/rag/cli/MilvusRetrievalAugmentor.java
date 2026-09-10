ffa34167dcd69eab58ed52fefce868ca8b4c56b1
kmos <kmos@commonhaus.dev>
2026-09-10 22:33:36 +0200

    add name to embedding model

--- rag/rag-cli/src/main/java/io/mosfet/rag/cli/MilvusRetrievalAugmentor.java
+++ rag/rag-cli/src/main/java/io/mosfet/rag/cli/MilvusRetrievalAugmentor.java
@@ -9,6 +9,7 @@ import dev.langchain4j.rag.DefaultRetrievalAugmentor;
 import dev.langchain4j.rag.RetrievalAugmentor;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.store.embedding.milvus.MilvusEmbeddingStore;
+import io.quarkiverse.langchain4j.ModelName;
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 
@@ -17,7 +18,7 @@ public class MilvusRetrievalAugmentor implements Supplier<RetrievalAugmentor> {
 
     private final RetrievalAugmentor augmentor;
 
-    MilvusRetrievalAugmentor(MilvusEmbeddingStore store, EmbeddingModel model) {
+    MilvusRetrievalAugmentor(MilvusEmbeddingStore store, @ModelName("granite") EmbeddingModel model) {
         EmbeddingStoreContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
                 .embeddingModel(model)
                 .embeddingStore(store)
